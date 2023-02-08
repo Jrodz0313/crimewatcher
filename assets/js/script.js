7ed7ddc24cb71fa5ca64d34e83eba45e3ee2c958
jayrussio10977 <120292190+jayrussio10977@users.noreply.github.com>
2023-02-07 19:22:18 -0500

    updated main

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,43 @@ fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/countries', options)
 	.catch(err => console.error(err));
 
     
+    function getDestinationId(location) {
+        const locale = "en-us"
+        
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'f952658629msh7726f90b2b0366fp176658jsn9a988e1b126b',
+                'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+            }
+        };
+        
+        var searchLocationsUrlString = "https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=en-us&name=" + location
+        
+        fetch(searchLocationsUrlString, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+        console.log(response)
+        var result = JSON.parse(response)
+        var destinationId = result[0].destinationId
+        console.log(destinationId)
+        // get destinatioId extracted 
+        return destinationId
+      }
+    
+    
+      
+    
+    getDestinationId("test")
+    
+        fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/countries', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    
+
+    
 
 
 let button = document.getElementById("get-location");
