d4d76885bc37c34bd062e6a9e4114e47003349f2
Mirek <nyziu@DESKTOP-N9P8O81>
2023-02-09 22:15:27 -0500

    updated css for fixed background image, better js comments in elements file

--- assets/js/elements.js
+++ assets/js/elements.js
@@ -2,14 +2,14 @@ var pageListings = document.getElementById('page-listings');
 
 
 function displayListing (listing) {
-  // Main listing container
+  //* Main listing container
   var listingBlock = document.createElement('div');
-      listingBlock.setAttribute('class', 'block has-background-light rounded p-2');
+      listingBlock.setAttribute('class', 'box has-background-light rounded p-2');
 
   var listingColumns = document.createElement('div');
       listingColumns.setAttribute('class', 'columns');
   
-    // Left side / image
+    //* Left side / image
     var listingImageContainer = document.createElement('div');
         listingImageContainer.setAttribute('class', 'column is-one-third-tablet');
     
@@ -19,26 +19,22 @@ function displayListing (listing) {
     var listingImage = document.createElement('img');
         listingImage.setAttribute('src', listing.max_photo_url);
         listingImage.setAttribute('alt', 'Hotel picture');
-        // listingImage.setAttribute('class', 'listing-link');
 
-        // Append listing block elements
+        //* Append listing block elements
         listingImageFigure.appendChild(listingImage);
         listingImageContainer.appendChild(listingImageFigure);
         listingColumns.appendChild(listingImageContainer);
         listingBlock.appendChild(listingColumns);
 
-
-
-
-
     
-    // Right side / content
+    
+    //* Right side / content
     var listingContentContainer = document.createElement('div');
         listingContentContainer.setAttribute('class', 'column is-three-quarter-desktop is-two-third-tablet');
 
 
 
-      // Level one left side
+      //* Level one left side
       var levelOne = document.createElement('div');
           levelOne.setAttribute('class', 'level is-mobile mb-0');
       
@@ -49,20 +45,20 @@ function displayListing (listing) {
           levelOneLeftItemOne.setAttribute('class', 'level-item');
 
       var listingName = document.createElement('a');
-          listingName.setAttribute('class', 'is-size-6-mobile is-size-6-tablet is-size-6-desktop has-text-weight-bold');
+          listingName.setAttribute('class', 'listingLink is-size-6-mobile is-size-6-tablet is-size-6-desktop has-text-weight-bold');
           listingName.setAttribute('href', listing.url);
-          listingName.setAttribute('target', '_blank');
+          listingName.setAttribute('target', '_blank');          
 
           listingName.innerHTML = listing.hotel_name;
 
-          // Append level one left side elements
+          //* Append level one left side elements
           levelOneLeftItemOne.appendChild(listingName);
           levelOneLeft.appendChild(levelOneLeftItemOne);
           levelOne.appendChild(levelOneLeft);
           listingContentContainer.appendChild(levelOne);
           listingColumns.appendChild(listingContentContainer);
         
-      // Level one right side
+      //* Level one right side
       var levelOneRight = document.createElement('div');
           levelOneRight.setAttribute('class', 'level-right');
 
@@ -71,16 +67,30 @@ function displayListing (listing) {
 
           levelOneRightItemOne.innerHTML = listing.review_score;
 
-          // Append level one right side elements
+          //* Append level one right side elements
           levelOneRight.appendChild(levelOneRightItemOne);
           levelOne.appendChild(levelOneRight);
 
+      //* Level two left side
+      var levelTwo = document.createElement('div');
+          levelTwo.setAttribute('class', 'level is-mobile mb-0');
+
+      var levelTwoLeft = document.createElement('div');
+          levelTwoLeft.setAttribute('class', 'level-left');
+
+      var levelTwoLeftItemOne = document.createElement('div');
+          levelTwoLeftItemOne.setAttribute('class', 'level-item');
+      
+      var levelTwoLeftItemOneP1 = document.createElement('p');
+          levelTwoLeftItemOneP1.setAttribute('class', 'subtitle is-6');
+          levelTwoLeftItemOneP1.innerHTML = listing.address + ', ' + listing.city; 
+
+          //* Append level two left side elements
+          levelTwoLeftItemOne.appendChild(levelTwoLeftItemOneP1);
+          levelTwoLeft.appendChild(levelTwoLeftItemOne)
 
 
 
-
-
-
-        // Append main listing section
+        //* Append main listing section
         pageListings.appendChild(listingBlock);
 }
